8ef20f2c44dd73ae1afa114b3040ad43c51adb3d
debuggerhu <debuggerhu@foxmail.com>
2020-06-04 11:57:47 +0800

    JVM学习的笔记代码

---
+++ jvm/src/com/hnbcit/jvm/Demo01.java
@@ -0,0 +1,20 @@
+package com.hnbcit.jvm;
+
+/**
+ * @author hhx
+ * @date 2020/5/21 23:06
+ * @description
+ */
+public class Demo01{
+    private String  name="zhangsan";
+    private String  username="zhangsanand ";
+    private int  age;
+//public static String  main(String[] args) {
+// return "heel";
+//}
+// public String method(String name,Integer age){
+//
+// return "hello";
+// }
+
+}
